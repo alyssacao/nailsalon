85832a52a0c34831fdf2287e72b25fd734932f75
Thi Thuy Dieu Cao <111800572+alyssacao@users.noreply.github.com>
2024-03-14 10:52:32 -0500

    Delete counteachservice:tech.sql

--- counteachservice:tech.sql
+++
@@ -1,9 +0,0 @@
-SELECT a.service, a.staffid, nt.name, a.service_count
-FROM (
-    SELECT service, staffid, DENSE_RANK() OVER (PARTITION BY service ORDER BY COUNT(*) DESC) AS service_rank,
-           COUNT(*) AS service_count
-    FROM appointment
-    GROUP BY service, staffid
-) a
-JOIN nailtechnician nt ON a.staffid = nt.staffid::INT
-WHERE a.service_rank = 1;
